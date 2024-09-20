e9b63a9423c44931a94559f39772c99494f2772e
khasmamad99 <sh.khasmamad@gmail.com>
2024-09-20 13:58:30 +0200

    add opt-125m complete model experiments

---
+++ examples/language-modeling/complete_model_experiment_scripts/opt_125m_lookahead.sh
@@ -0,0 +1,97 @@
+python3 main.py \
+    --model_name facebook/opt-125m  \
+    --bits 4 \
+    --group_size 128 \
+    --enable_lr_scheduler \
+    --nsamples 8192 \
+    --iters 1000 \
+    --nblocks 1 \
+    --num_lookahead_blocks 5 \
+    --tasks wikitext \
+    --low_gpu_mem_usage \
+    --wandb_project_name khas-thesis-autoround-complete-model &
+
+python3 main.py \
+    --model_name facebook/opt-125m  \
+    --bits 4 \
+    --group_size 128 \
+    --enable_lr_scheduler \
+    --nsamples 8192 \
+    --iters 1000 \
+    --nblocks 1 \
+    --num_lookahead_blocks 6 \
+    --tasks wikitext \
+    --low_gpu_mem_usage \
+    --wandb_project_name khas-thesis-autoround-complete-model
+
+wait
+
+python3 main.py \
+    --model_name facebook/opt-125m  \
+    --bits 4 \
+    --group_size 128 \
+    --enable_lr_scheduler \
+    --nsamples 8192 \
+    --iters 1000 \
+    --nblocks 1 \
+    --num_lookahead_blocks 7 \
+    --tasks wikitext \
+    --low_gpu_mem_usage \
+    --wandb_project_name khas-thesis-autoround-complete-model &
+
+python3 main.py \
+    --model_name facebook/opt-125m  \
+    --bits 4 \
+    --group_size 128 \
+    --enable_lr_scheduler \
+    --nsamples 8192 \
+    --iters 1000 \
+    --nblocks 1 \
+    --num_lookahead_blocks 8 \
+    --tasks wikitext \
+    --low_gpu_mem_usage \
+    --wandb_project_name khas-thesis-autoround-complete-model
+
+wait
+
+python3 main.py \
+    --model_name facebook/opt-125m  \
+    --bits 4 \
+    --group_size 128 \
+    --enable_lr_scheduler \
+    --nsamples 8192 \
+    --iters 25 --disable_wandb \
+    --nblocks 1 \
+    --num_lookahead_blocks 9 \
+    --tasks wikitext \
+    --low_gpu_mem_usage \
+    --wandb_project_name khas-thesis-autoround-complete-model
+
+
+python3 main.py \
+    --model_name facebook/opt-125m  \
+    --bits 4 \
+    --group_size 128 \
+    --enable_lr_scheduler \
+    --nsamples 8192 \
+    --iters 25 --disable_wandb \
+    --nblocks 1 \
+    --num_lookahead_blocks 10 \
+    --tasks wikitext \
+    --low_gpu_mem_usage \
+    --wandb_project_name khas-thesis-autoround-complete-model
+
+
+python3 main.py \
+    --model_name facebook/opt-125m  \
+    --bits 4 \
+    --group_size 128 \
+    --enable_lr_scheduler \
+    --nsamples 8192 \
+    --iters 1000 \
+    --nblocks 1 \
+    --num_lookahead_blocks 11 \
+    --tasks wikitext \
+    --low_gpu_mem_usage \
+    --wandb_project_name khas-thesis-autoround-complete-model
+
---
+++ examples/language-modeling/complete_model_experiment_scripts/opt_125m_multiblock.sh
@@ -0,0 +1,97 @@
+python3 main.py \
+    --model_name facebook/opt-125m  \
+    --bits 4 \
+    --group_size 128 \
+    --enable_lr_scheduler \
+    --nsamples 8192 \
+    --iters 1000 \
+    --nblocks 6 \
+    --num_lookahead_blocks 0 \
+    --tasks wikitext \
+    --low_gpu_mem_usage \
+    --wandb_project_name khas-thesis-autoround-complete-model &
+
+python3 main.py \
+    --model_name facebook/opt-125m  \
+    --bits 4 \
+    --group_size 128 \
+    --enable_lr_scheduler \
+    --nsamples 8192 \
+    --iters 1000 \
+    --nblocks 7 \
+    --num_lookahead_blocks 0 \
+    --tasks wikitext \
+    --low_gpu_mem_usage \
+    --wandb_project_name khas-thesis-autoround-complete-model
+
+wait
+
+python3 main.py \
+    --model_name facebook/opt-125m  \
+    --bits 4 \
+    --group_size 128 \
+    --enable_lr_scheduler \
+    --nsamples 8192 \
+    --iters 1000 \
+    --nblocks 8 \
+    --num_lookahead_blocks 0 \
+    --tasks wikitext \
+    --low_gpu_mem_usage \
+    --wandb_project_name khas-thesis-autoround-complete-model &
+
+python3 main.py \
+    --model_name facebook/opt-125m  \
+    --bits 4 \
+    --group_size 128 \
+    --enable_lr_scheduler \
+    --nsamples 8192 \
+    --iters 1000 \
+    --nblocks 9 \
+    --num_lookahead_blocks 0 \
+    --tasks wikitext \
+    --low_gpu_mem_usage \
+    --wandb_project_name khas-thesis-autoround-complete-model
+
+wait
+
+python3 main.py \
+    --model_name facebook/opt-125m  \
+    --bits 4 \
+    --group_size 128 \
+    --enable_lr_scheduler \
+    --nsamples 8192 \
+    --iters 25 --disable_wandb \
+    --nblocks 10 \
+    --num_lookahead_blocks 0 \
+    --tasks wikitext \
+    --low_gpu_mem_usage \
+    --wandb_project_name khas-thesis-autoround-complete-model
+
+
+python3 main.py \
+    --model_name facebook/opt-125m  \
+    --bits 4 \
+    --group_size 128 \
+    --enable_lr_scheduler \
+    --nsamples 8192 \
+    --iters 25 --disable_wandb \
+    --nblocks 11 \
+    --num_lookahead_blocks 0 \
+    --tasks wikitext \
+    --low_gpu_mem_usage \
+    --wandb_project_name khas-thesis-autoround-complete-model
+
+
+python3 main.py \
+    --model_name facebook/opt-125m  \
+    --bits 4 \
+    --group_size 128 \
+    --enable_lr_scheduler \
+    --nsamples 8192 \
+    --iters 1000 \
+    --nblocks 12 \
+    --num_lookahead_blocks 0 \
+    --tasks wikitext \
+    --low_gpu_mem_usage \
+    --wandb_project_name khas-thesis-autoround-complete-model
+
