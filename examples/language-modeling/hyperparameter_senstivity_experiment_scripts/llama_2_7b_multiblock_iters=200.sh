e404146cf0d6a994072c5b804896655086c39a51
khasmamad99 <sh.khasmamad@gmail.com>
2024-09-20 11:06:34 +0200

    add hypeparam sensitivity experiment scripts

---
+++ examples/language-modeling/hyperparameter_senstivity_experiment_scripts/llama_2_7b_multiblock_iters=200.sh
@@ -0,0 +1,52 @@
+python3 main.py \
+    --model_name meta-llama/llama-2-7b-hf  \
+    --bits 4 \
+    --group_size 128 \
+    --lr 1e-3 \
+    --enable_lr_scheduler \
+    --nsamples 512 \
+    --iters 200 \
+    --nblocks 2 \
+    --num_lookahead_blocks 0 \
+    --tasks wikitext \
+    --wandb_project_name khas-thesis-autoround-hyperparam-sensitivity
+
+python3 main.py \
+    --model_name meta-llama/llama-2-7b-hf  \
+    --bits 4 \
+    --group_size 128 \
+    --lr 1e-3 \
+    --enable_lr_scheduler \
+    --nsamples 512 \
+    --iters 200 \
+    --nblocks 3 \
+    --num_lookahead_blocks 0 \
+    --tasks wikitext \
+    --wandb_project_name khas-thesis-autoround-hyperparam-sensitivity
+
+python3 main.py \
+    --model_name meta-llama/llama-2-7b-hf  \
+    --bits 4 \
+    --group_size 128 \
+    --lr 1e-3 \
+    --enable_lr_scheduler \
+    --nsamples 512 \
+    --iters 200 \
+    --nblocks 4 \
+    --num_lookahead_blocks 0 \
+    --tasks wikitext \
+    --wandb_project_name khas-thesis-autoround-hyperparam-sensitivity
+
+python3 main.py \
+    --model_name meta-llama/llama-2-7b-hf  \
+    --bits 4 \
+    --group_size 128 \
+    --lr 1e-3 \
+    --enable_lr_scheduler \
+    --nsamples 512 \
+    --iters 200 \
+    --nblocks 5 \
+    --num_lookahead_blocks 0 \
+    --tasks wikitext \
+    --wandb_project_name khas-thesis-autoround-hyperparam-sensitivity \
+    --low_gpu_mem_usage
