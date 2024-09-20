a7d4e81dd3a173e300b3b0fc78f8c474026f51e5
khasmamad99 <sh.khasmamad@gmail.com>
2024-09-20 11:12:01 +0200

    add mistral 7b main experiment scripts

---
+++ examples/language-modeling/main_experiment_scripts/mistral_7b_multiblock.sh
@@ -0,0 +1,44 @@
+python3 main.py \
+    --model_name mistralai/Mistral-7B-v0.1  \
+    --bits 4 \
+    --group_size 128 \
+    --enable_lr_scheduler \
+    --nsamples 512 \
+    --iters 1000 \
+    --nblocks 2 \
+    --num_lookahead_blocks 0 \
+    --tasks wikitext
+
+python3 main.py \
+    --model_name mistralai/Mistral-7B-v0.1  \
+    --bits 4 \
+    --group_size 128 \
+    --enable_lr_scheduler \
+    --nsamples 512 \
+    --iters 1000 \
+    --nblocks 3 \
+    --num_lookahead_blocks 0 \
+    --tasks wikitext
+
+python3 main.py \
+    --model_name mistralai/Mistral-7B-v0.1  \
+    --bits 4 \
+    --group_size 128 \
+    --enable_lr_scheduler \
+    --nsamples 512 \
+    --iters 1000 \
+    --nblocks 4 \
+    --num_lookahead_blocks 0 \
+    --tasks wikitext
+
+python3 main.py \
+    --model_name mistralai/Mistral-7B-v0.1  \
+    --bits 4 \
+    --group_size 128 \
+    --enable_lr_scheduler \
+    --nsamples 512 \
+    --iters 1000 \
+    --nblocks 5 \
+    --num_lookahead_blocks 0 \
+    --tasks wikitext \
+    --low_gpu_mem_usage
