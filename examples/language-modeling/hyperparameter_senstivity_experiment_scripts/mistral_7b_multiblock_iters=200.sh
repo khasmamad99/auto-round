ebbb7289fb47812b3f4d740ba0b85dd5bd409d09
khasmamad99 <sh.khasmamad@gmail.com>
2024-09-20 17:39:31 +0200

    add mistral 7b hyperparam sensitivity experiments

---
+++ examples/language-modeling/hyperparameter_senstivity_experiment_scripts/mistral_7b_multiblock_iters=200.sh
@@ -0,0 +1,52 @@
+python3 main.py \
+    --model_name mistralai/Mistral-7B-v0.1  \
+    --bits 4 \
+    --group_size 128 \
+    --lr 1e-3 \
+    --enable_lr_scheduler \
+    --nsamples 512 \
+    --iters 200 \
+    --nblocks 2 \
+    --num_lookahead_blocks 0 \
+    --tasks wikitext \
+    --wandb_project_name khas-thesis-autoround-hyperparam-sensitivity
+
+python3 main.py \
+    --model_name mistralai/Mistral-7B-v0.1  \
+    --bits 4 \
+    --group_size 128 \
+    --lr 1e-3 \
+    --enable_lr_scheduler \
+    --nsamples 512 \
+    --iters 200 \
+    --nblocks 3 \
+    --num_lookahead_blocks 0 \
+    --tasks wikitext \
+    --wandb_project_name khas-thesis-autoround-hyperparam-sensitivity
+
+python3 main.py \
+    --model_name mistralai/Mistral-7B-v0.1  \
+    --bits 4 \
+    --group_size 128 \
+    --lr 1e-3 \
+    --enable_lr_scheduler \
+    --nsamples 512 \
+    --iters 200 \
+    --nblocks 4 \
+    --num_lookahead_blocks 0 \
+    --tasks wikitext \
+    --wandb_project_name khas-thesis-autoround-hyperparam-sensitivity
+
+python3 main.py \
+    --model_name mistralai/Mistral-7B-v0.1  \
+    --bits 4 \
+    --group_size 128 \
+    --lr 1e-3 \
+    --enable_lr_scheduler \
+    --nsamples 512 \
+    --iters 200 \
+    --nblocks 5 \
+    --num_lookahead_blocks 0 \
+    --tasks wikitext \
+    --wandb_project_name khas-thesis-autoround-hyperparam-sensitivity \
+    --low_gpu_mem_usage
