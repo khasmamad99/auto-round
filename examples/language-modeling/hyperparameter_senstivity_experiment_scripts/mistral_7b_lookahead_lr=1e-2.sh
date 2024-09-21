cfd169aa9fb2cf2d45776193e3223da101ddf06f
khasmamad99 <sh.khasmamad@gmail.com>
2024-09-21 20:18:11 +0200

    add more hyperparameter sensitivty scripts

---
+++ examples/language-modeling/hyperparameter_senstivity_experiment_scripts/mistral_7b_lookahead_lr=1e-2.sh
@@ -0,0 +1,64 @@
+python3 main.py \
+    --model_name mistralai/Mistral-7B-v0.1  \
+    --bits 4 \
+    --group_size 128 \
+    --lr 1e-2 \
+    --enable_lr_scheduler \
+    --nsamples 512 \
+    --iters 1000 \
+    --nblocks 1 \
+    --num_lookahead_blocks 0 \
+    --tasks wikitext \
+    --wandb_project_name khas-thesis-autoround-hyperparam-sensitivity 
+
+python3 main.py \
+    --model_name mistralai/Mistral-7B-v0.1  \
+    --bits 4 \
+    --group_size 128 \
+    --lr 1e-2 \
+    --enable_lr_scheduler \
+    --nsamples 512 \
+    --iters 1000 \
+    --nblocks 1 \
+    --num_lookahead_blocks 1 \
+    --tasks wikitext \
+    --wandb_project_name khas-thesis-autoround-hyperparam-sensitivity
+
+python3 main.py \
+    --model_name mistralai/Mistral-7B-v0.1  \
+    --bits 4 \
+    --group_size 128 \
+    --lr 1e-2 \
+    --enable_lr_scheduler \
+    --nsamples 512 \
+    --iters 1000 \
+    --nblocks 1 \
+    --num_lookahead_blocks 2 \
+    --tasks wikitext \
+    --wandb_project_name khas-thesis-autoround-hyperparam-sensitivity
+
+python3 main.py \
+    --model_name mistralai/Mistral-7B-v0.1  \
+    --bits 4 \
+    --group_size 128 \
+    --lr 1e-2 \
+    --enable_lr_scheduler \
+    --nsamples 512 \
+    --iters 10 --disable_wandb \
+    --nblocks 1 \
+    --num_lookahead_blocks 3 \
+    --tasks wikitext \
+    --wandb_project_name khas-thesis-autoround-hyperparam-sensitivity
+
+python3 main.py \
+    --model_name mistralai/Mistral-7B-v0.1  \
+    --bits 4 \
+    --group_size 128 \
+    --lr 1e-2 \
+    --enable_lr_scheduler \
+    --nsamples 512 \
+    --iters 1000 \
+    --nblocks 1 \
+    --num_lookahead_blocks 4 \
+    --tasks wikitext \
+    --wandb_project_name khas-thesis-autoround-hyperparam-sensitivity
---
+++ examples/language-modeling/hyperparameter_senstivity_experiment_scripts/opt_6_7b_lookahead_nsampels=128.sh
@@ -0,0 +1,64 @@
+python3 main.py \
+    --model_name facebook/opt-6.7b  \
+    --bits 4 \
+    --group_size 128 \
+    --lr 1e-3 \
+    --enable_lr_scheduler \
+    --nsamples 128 \
+    --iters 1000 \
+    --nblocks 1 \
+    --num_lookahead_blocks 0 \
+    --tasks wikitext \
+    --wandb_project_name khas-thesis-autoround-hyperparam-sensitivity
+
+python3 main.py \
+    --model_name facebook/opt-6.7b  \
+    --bits 4 \
+    --group_size 128 \
+    --lr 1e-3 \
+    --enable_lr_scheduler \
+    --nsamples 128 \
+    --iters 1000 \
+    --nblocks 1 \
+    --num_lookahead_blocks 1 \
+    --tasks wikitext \
+    --wandb_project_name khas-thesis-autoround-hyperparam-sensitivity
+
+python3 main.py \
+    --model_name facebook/opt-6.7b  \
+    --bits 4 \
+    --group_size 128 \
+    --lr 1e-3 \
+    --enable_lr_scheduler \
+    --nsamples 128 \
+    --iters 1000 \
+    --nblocks 1 \
+    --num_lookahead_blocks 2 \
+    --tasks wikitext \
+    --wandb_project_name khas-thesis-autoround-hyperparam-sensitivity
+
+python3 main.py \
+    --model_name facebook/opt-6.7b  \
+    --bits 4 \
+    --group_size 128 \
+    --lr 1e-3 \
+    --enable_lr_scheduler \
+    --nsamples 128 \
+    --iters 1000 \
+    --nblocks 1 \
+    --num_lookahead_blocks 3 \
+    --tasks wikitext \
+    --wandb_project_name khas-thesis-autoround-hyperparam-sensitivity
+
+python3 main.py \
+    --model_name facebook/opt-6.7b  \
+    --bits 4 \
+    --group_size 128 \
+    --lr 1e-3 \
+    --enable_lr_scheduler \
+    --nsamples 128 \
+    --iters 1000 \
+    --nblocks 1 \
+    --num_lookahead_blocks 4 \
+    --tasks wikitext \
+    --wandb_project_name khas-thesis-autoround-hyperparam-sensitivity
