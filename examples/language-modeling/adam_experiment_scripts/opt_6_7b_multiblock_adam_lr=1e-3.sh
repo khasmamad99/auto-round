ba02f339a13dbadb25574a0e1c1fc463583f9b23
khasmamad99 <sh.khasmamad@gmail.com>
2024-09-22 11:04:55 +0200

    add opt 6.7b adam experiment scripts

---
+++ examples/language-modeling/adam_experiment_scripts/opt_6_7b_multiblock_adam_lr=1e-3.sh
@@ -0,0 +1,59 @@
+python3 main.py \
+    --model_name facebook/opt-6.7b  \
+    --bits 4 \
+    --group_size 128 \
+    --lr 1e-3 \
+    --enable_lr_scheduler \
+    --nsamples 512 \
+    --iters 1000 \
+    --adam \
+    --nblocks 2 \
+    --num_lookahead_blocks 0 \
+    --tasks wikitext \
+    --wandb_project_name khas-thesis-autoround-hyperparam-sensitivity \
+    --low_gpu_mem_usage
+
+python3 main.py \
+    --model_name facebook/opt-6.7b  \
+    --bits 4 \
+    --group_size 128 \
+    --lr 1e-3 \
+    --enable_lr_scheduler \
+    --nsamples 512 \
+    --iters 1000 \
+    --adam \
+    --nblocks 3 \
+    --num_lookahead_blocks 0 \
+    --tasks wikitext \
+    --wandb_project_name khas-thesis-autoround-hyperparam-sensitivity \
+    --low_gpu_mem_usage
+
+python3 main.py \
+    --model_name facebook/opt-6.7b  \
+    --bits 4 \
+    --group_size 128 \
+    --lr 1e-3 \
+    --enable_lr_scheduler \
+    --nsamples 512 \
+    --iters 1000 \
+    --adam \
+    --nblocks 4 \
+    --num_lookahead_blocks 0 \
+    --tasks wikitext \
+    --wandb_project_name khas-thesis-autoround-hyperparam-sensitivity \
+    --low_gpu_mem_usage
+
+python3 main.py \
+    --model_name facebook/opt-6.7b  \
+    --bits 4 \
+    --group_size 128 \
+    --lr 1e-3 \
+    --enable_lr_scheduler \
+    --nsamples 512 \
+    --iters 1000 \
+    --adam \
+    --nblocks 5 \
+    --num_lookahead_blocks 0 \
+    --tasks wikitext \
+    --wandb_project_name khas-thesis-autoround-hyperparam-sensitivity \
+    --low_gpu_mem_usage
