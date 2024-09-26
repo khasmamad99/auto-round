1936e03bed7851f0f71522423fd7706c9908ab3b
khasmamad99 <sh.khasmamad@gmail.com>
2024-09-26 22:42:03 +0200

    add cbq scripts

---
+++ examples/language-modeling/main_experiment_scripts/v2/opt_125m_cbq.sh
@@ -0,0 +1,152 @@
+python3 main.py \
+    --model_name facebook/opt-125m  \
+    --bits 4 \
+    --group_size 128 \
+    --eval_bs 32 \
+    --enable_lr_scheduler \
+    --nsamples 512 \
+    --iters 1000 \
+    --nblocks 2 \
+    --block_step_size 1 \
+    --num_lookahead_blocks 0 \
+    --wandb_project_name khas-thesis-main &
+
+python3 main.py \
+    --model_name facebook/opt-125m  \
+    --bits 4 \
+    --group_size 128 \
+    --eval_bs 32 \
+    --enable_lr_scheduler \
+    --nsamples 512 \
+    --iters 1000 \
+    --nblocks 3 \
+    --block_step_size 1 \
+    --num_lookahead_blocks 0 \
+    --wandb_project_name khas-thesis-main
+
+wait
+
+
+python3 main.py \
+    --model_name facebook/opt-125m  \
+    --bits 4 \
+    --group_size 128 \
+    --eval_bs 32 \
+    --enable_lr_scheduler \
+    --nsamples 512 \
+    --iters 1000 \
+    --nblocks 4 \
+    --block_step_size 1 \
+    --num_lookahead_blocks 0 \
+    --wandb_project_name khas-thesis-main &
+
+python3 main.py \
+    --model_name facebook/opt-125m  \
+    --bits 4 \
+    --group_size 128 \
+    --eval_bs 32 \
+    --enable_lr_scheduler \
+    --nsamples 512 \
+    --iters 1000 \
+    --nblocks 5 \
+    --block_step_size 1 \
+    --num_lookahead_blocks 0 \
+    --wandb_project_name khas-thesis-main
+
+wait
+
+
+python3 main.py \
+    --model_name facebook/opt-125m  \
+    --bits 4 \
+    --group_size 128 \
+    --eval_bs 32 \
+    --enable_lr_scheduler \
+    --nsamples 512 \
+    --iters 1000 \
+    --nblocks 6 \
+    --block_step_size 1 \
+    --num_lookahead_blocks 0 \
+    --wandb_project_name khas-thesis-main
+
+python3 main.py \
+    --model_name facebook/opt-125m  \
+    --bits 4 \
+    --group_size 128 \
+    --eval_bs 32 \
+    --enable_lr_scheduler \
+    --nsamples 512 \
+    --iters 1000 \
+    --nblocks 7 \
+    --block_step_size 1 \
+    --num_lookahead_blocks 0 \
+    --wandb_project_name khas-thesis-main
+
+
+python3 main.py \
+    --model_name facebook/opt-125m  \
+    --bits 4 \
+    --group_size 128 \
+    --eval_bs 32 \
+    --enable_lr_scheduler \
+    --nsamples 512 \
+    --iters 1000 \
+    --nblocks 8 \
+    --block_step_size 1 \
+    --num_lookahead_blocks 0 \
+    --wandb_project_name khas-thesis-main
+
+python3 main.py \
+    --model_name facebook/opt-125m  \
+    --bits 4 \
+    --group_size 128 \
+    --eval_bs 32 \
+    --enable_lr_scheduler \
+    --nsamples 512 \
+    --iters 1000 \
+    --nblocks 9 \
+    --block_step_size 1 \
+    --num_lookahead_blocks 0 \
+    --wandb_project_name khas-thesis-main
+
+
+python3 main.py \
+    --model_name facebook/opt-125m  \
+    --bits 4 \
+    --group_size 128 \
+    --eval_bs 32 \
+    --enable_lr_scheduler \
+    --nsamples 512 \
+    --iters 1000 \
+    --nblocks 10 \
+    --block_step_size 10 \
+    --num_lookahead_blocks 0 \
+    --wandb_project_name khas-thesis-main
+
+
+python3 main.py \
+    --model_name facebook/opt-125m  \
+    --bits 4 \
+    --group_size 128 \
+    --eval_bs 32 \
+    --enable_lr_scheduler \
+    --nsamples 512 \
+    --iters 1000 \
+    --nblocks 11 \
+    --block_step_size 11 \
+    --num_lookahead_blocks 0 \
+    --wandb_project_name khas-thesis-main
+
+
+python3 main.py \
+    --model_name facebook/opt-125m  \
+    --bits 4 \
+    --group_size 128 \
+    --eval_bs 32 \
+    --enable_lr_scheduler \
+    --nsamples 512 \
+    --iters 1000 \
+    --nblocks 12 \
+    --block_step_size 12 \
+    --num_lookahead_blocks 0 \
+    --wandb_project_name khas-thesis-main
---
+++ examples/language-modeling/main_experiment_scripts/v2/opt_6_7b_cbq.sh
@@ -0,0 +1,55 @@
+python3 main.py \
+    --model_name facebook/opt-6.7b  \
+    --bits 4 \
+    --group_size 128 \
+    --eval_bs 32 \
+    --enable_lr_scheduler \
+    --nsamples 512 \
+    --iters 1000 \
+    --nblocks 2 \
+    --block_step_size 1 \
+    --num_lookahead_blocks 0 \
+    --wandb_project_name khas-thesis-main \
+    --low_gpu_mem_usage
+
+python3 main.py \
+    --model_name facebook/opt-6.7b  \
+    --bits 4 \
+    --group_size 128 \
+    --eval_bs 32 \
+    --enable_lr_scheduler \
+    --nsamples 512 \
+    --iters 1000 \
+    --nblocks 3 \
+    --block_step_size 1 \
+    --num_lookahead_blocks 0 \
+    --wandb_project_name khas-thesis-main \
+    --low_gpu_mem_usage
+
+python3 main.py \
+    --model_name facebook/opt-6.7b  \
+    --bits 4 \
+    --group_size 128 \
+    --eval_bs 32 \
+    --enable_lr_scheduler \
+    --nsamples 512 \
+    --iters 1000 \
+    --nblocks 4 \
+    --block_step_size 1 \
+    --num_lookahead_blocks 0 \
+    --wandb_project_name khas-thesis-main \
+    --low_gpu_mem_usage
+
+# python3 main.py \
+#     --model_name facebook/opt-6.7b  \
+#     --bits 4 \
+#     --group_size 128 \
+#     --eval_bs 32 \
+#     --enable_lr_scheduler \
+#     --nsamples 512 \
+#     --iters 1000 \
+#     --nblocks 5 \
+#     --block_step_size 1 \
+#     --num_lookahead_blocks 0 \
+#     --wandb_project_name khas-thesis-main \
+#     --low_gpu_mem_usage
