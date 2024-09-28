c30cf45a0117c8fc9922ae1d438952d04c588629
khasmamad99 <sh.khasmamad@gmail.com>
2024-09-28 17:06:29 +0200

    add opt 6.7b nsamples ablation script

---
+++ examples/language-modeling/ablation_experiments_scripts/opt_6_7b_nsamples.sh
@@ -0,0 +1,94 @@
+python3 main.py \
+    --model_name facebook/opt-6.7b  \
+    --bits 4 \
+    --group_size 128 \
+    --eval_bs 32 \
+    --lr 1e-3 \
+    --enable_lr_scheduler \
+    --nsamples 128 \
+    --iters 1000 \
+    --nblocks 1 \
+    --block_step_size 1 \
+    --num_lookahead_blocks 3 \
+    --low_gpu_mem_usage \
+    --wandb_project_name khas-thesis-ablations
+
+
+python3 main.py \
+    --model_name facebook/opt-6.7b  \
+    --bits 4 \
+    --group_size 128 \
+    --eval_bs 32 \
+    --lr 1e-3 \
+    --enable_lr_scheduler \
+    --nsamples 1024 \
+    --iters 1000 \
+    --nblocks 1 \
+    --block_step_size 1 \
+    --num_lookahead_blocks 3 \
+    --low_gpu_mem_usage \
+    --wandb_project_name khas-thesis-ablations
+
+
+python3 main.py \
+    --model_name facebook/opt-6.7b  \
+    --bits 4 \
+    --group_size 128 \
+    --eval_bs 32 \
+    --lr 1e-3 \
+    --enable_lr_scheduler \
+    --nsamples 128 \
+    --iters 1000 \
+    --nblocks 3 \
+    --block_step_size 3 \
+    --num_lookahead_blocks 0 \
+    --low_gpu_mem_usage \
+    --wandb_project_name khas-thesis-ablations
+
+
+python3 main.py \
+    --model_name facebook/opt-6.7b  \
+    --bits 4 \
+    --group_size 128 \
+    --eval_bs 32 \
+    --lr 1e-3 \
+    --enable_lr_scheduler \
+    --nsamples 1024 \
+    --iters 1000 \
+    --nblocks 3 \
+    --block_step_size 3 \
+    --num_lookahead_blocks 0 \
+    --low_gpu_mem_usage \
+    --wandb_project_name khas-thesis-ablations
+
+
+python3 main.py \
+    --model_name facebook/opt-6.7b  \
+    --bits 4 \
+    --group_size 128 \
+    --eval_bs 32 \
+    --lr 1e-3 \
+    --enable_lr_scheduler \
+    --nsamples 128 \
+    --iters 1000 \
+    --nblocks 3 \
+    --block_step_size 1 \
+    --num_lookahead_blocks 0 \
+    --low_gpu_mem_usage \
+    --wandb_project_name khas-thesis-ablations
+
+
+python3 main.py \
+    --model_name facebook/opt-6.7b  \
+    --bits 4 \
+    --group_size 128 \
+    --eval_bs 32 \
+    --lr 1e-3 \
+    --enable_lr_scheduler \
+    --nsamples 1024 \
+    --iters 1000 \
+    --nblocks 3 \
+    --block_step_size 1 \
+    --num_lookahead_blocks 0 \
+    --low_gpu_mem_usage \
+    --wandb_project_name khas-thesis-ablations
