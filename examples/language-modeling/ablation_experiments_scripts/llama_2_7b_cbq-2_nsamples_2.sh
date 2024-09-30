ff20edcd83b9182acc34f4cbf50b720933d204b0
khasmamad99 <sh.khasmamad@gmail.com>
2024-09-30 13:10:58 +0200

    add llama_2_7b nsamples sweep scripts

---
+++ examples/language-modeling/ablation_experiments_scripts/llama_2_7b_cbq-2_nsamples_2.sh
@@ -0,0 +1,107 @@
+# python3 main.py \
+#     --model_name meta-llama/llama-2-7b-hf  \
+#     --bits 4 \
+#     --group_size 128 \
+#     --eval_bs 32 \
+#     --lr 1e-3 \
+#     --enable_lr_scheduler \
+#     --nsamples 1411 \
+#     --iters 1000 \
+#     --nblocks 2 \
+#     --block_step_size 1 \
+#     --num_lookahead_blocks 0 \
+#     --wandb_project_name khas-thesis-ablations \
+#     --low_gpu_mem_usage
+
+
+python3 main.py \
+    --model_name meta-llama/llama-2-7b-hf  \
+    --bits 4 \
+    --group_size 128 \
+    --eval_bs 32 \
+    --lr 1e-3 \
+    --enable_lr_scheduler \
+    --nsamples 1024 \
+    --iters 1000 \
+    --nblocks 2 \
+    --block_step_size 1 \
+    --num_lookahead_blocks 0 \
+    --wandb_project_name khas-thesis-ablations \
+    --low_gpu_mem_usage
+
+
+python3 main.py \
+    --model_name meta-llama/llama-2-7b-hf  \
+    --bits 4 \
+    --group_size 128 \
+    --eval_bs 32 \
+    --lr 1e-3 \
+    --enable_lr_scheduler \
+    --nsamples 768 \
+    --iters 1000 \
+    --nblocks 2 \
+    --block_step_size 1 \
+    --num_lookahead_blocks 0 \
+    --wandb_project_name khas-thesis-ablations \
+    --low_gpu_mem_usage
+
+
+
+# python3 main.py \
+#     --model_name meta-llama/llama-2-7b-hf  \
+#     --bits 4 \
+#     --group_size 128 \
+#     --eval_bs 32 \
+#     --lr 1e-3 \
+#     --enable_lr_scheduler \
+#     --nsamples 256 \
+#     --iters 1000 \
+#     --nblocks 2 \
+#     --block_step_size 1 \
+#     --num_lookahead_blocks 0 \
+#     --wandb_project_name khas-thesis-ablations
+
+
+# python3 main.py \
+#     --model_name meta-llama/llama-2-7b-hf  \
+#     --bits 4 \
+#     --group_size 128 \
+#     --eval_bs 32 \
+#     --lr 1e-3 \
+#     --enable_lr_scheduler \
+#     --nsamples 128 \
+#     --iters 1000 \
+#     --nblocks 2 \
+#     --block_step_size 1 \
+#     --num_lookahead_blocks 0 \
+#     --wandb_project_name khas-thesis-ablations
+
+
+# python3 main.py \
+#     --model_name meta-llama/llama-2-7b-hf  \
+#     --bits 4 \
+#     --group_size 128 \
+#     --eval_bs 32 \
+#     --lr 1e-3 \
+#     --enable_lr_scheduler \
+#     --nsamples 1024 \
+#     --iters 1000 \
+#     --nblocks 2 \
+#     --block_step_size 1 \
+#     --num_lookahead_blocks 0 \
+#     --wandb_project_name khas-thesis-ablations
+
+
+# python3 main.py \
+#     --model_name meta-llama/llama-2-7b-hf  \
+#     --bits 4 \
+#     --group_size 128 \
+#     --eval_bs 32 \
+#     --lr 1e-3 \
+#     --enable_lr_scheduler \
+#     --nsamples 512 \
+#     --iters 1000 \
+#     --nblocks 2 \
+#     --block_step_size 1 \
+#     --num_lookahead_blocks 0 \
+#     --wandb_project_name khas-thesis-ablations
