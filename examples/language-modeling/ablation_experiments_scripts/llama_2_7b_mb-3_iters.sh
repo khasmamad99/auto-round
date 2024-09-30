5fe7c18df234f0624dac47381ba630c908296075
khasmamad99 <sh.khasmamad@gmail.com>
2024-09-30 11:51:51 +0200

    add llama_2_7b iters sweep scripts

---
+++ examples/language-modeling/ablation_experiments_scripts/llama_2_7b_mb-3_iters.sh
@@ -0,0 +1,88 @@
+python3 main.py \
+    --model_name meta-llama/llama-2-7b-hf  \
+    --bits 4 \
+    --group_size 128 \
+    --eval_bs 32 \
+    --lr 1e-3 \
+    --enable_lr_scheduler \
+    --nsamples 512 \
+    --iters 50 \
+    --nblocks 3 \
+    --block_step_size 3 \
+    --num_lookahead_blocks 0 \
+    --wandb_project_name khas-thesis-ablations
+    
+
+python3 main.py \
+    --model_name meta-llama/llama-2-7b-hf  \
+    --bits 4 \
+    --group_size 128 \
+    --eval_bs 32 \
+    --lr 1e-3 \
+    --enable_lr_scheduler \
+    --nsamples 512 \
+    --iters 100 \
+    --nblocks 3 \
+    --block_step_size 3 \
+    --num_lookahead_blocks 0 \
+    --wandb_project_name khas-thesis-ablations
+
+
+# python3 main.py \
+#     --model_name meta-llama/llama-2-7b-hf  \
+#     --bits 4 \
+#     --group_size 128 \
+#     --eval_bs 32 \
+#     --lr 1e-3 \
+#     --enable_lr_scheduler \
+#     --nsamples 512 \
+#     --iters 200 \
+#     --nblocks 3 \
+#     --block_step_size 3 \
+#     --num_lookahead_blocks 0 \
+#     --wandb_project_name khas-thesis-ablations
+
+
+python3 main.py \
+    --model_name meta-llama/llama-2-7b-hf  \
+    --bits 4 \
+    --group_size 128 \
+    --eval_bs 32 \
+    --lr 1e-3 \
+    --enable_lr_scheduler \
+    --nsamples 512 \
+    --iters 500 \
+    --nblocks 3 \
+    --block_step_size 3 \
+    --num_lookahead_blocks 0 \
+    --wandb_project_name khas-thesis-ablations
+
+
+python3 main.py \
+    --model_name meta-llama/llama-2-7b-hf  \
+    --bits 4 \
+    --group_size 128 \
+    --eval_bs 32 \
+    --lr 1e-3 \
+    --enable_lr_scheduler \
+    --nsamples 512 \
+    --iters 750 \
+    --nblocks 3 \
+    --block_step_size 3 \
+    --num_lookahead_blocks 0 \
+    --wandb_project_name khas-thesis-ablations
+
+
+# python3 main.py \
+#     --model_name meta-llama/llama-2-7b-hf  \
+#     --bits 4 \
+#     --group_size 128 \
+#     --eval_bs 32 \
+#     --lr 1e-3 \
+#     --enable_lr_scheduler \
+#     --nsamples 512 \
+#     --iters 1200 \
+#     --nblocks 3 \
+#     --block_step_size 3 \
+#     --num_lookahead_blocks 0 \
+#     --wandb_project_name khas-thesis-ablations
