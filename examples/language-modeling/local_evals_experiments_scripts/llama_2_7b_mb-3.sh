783467bd2e176a28e217f67fc7b576906672010f
khasmamad99 <sh.khasmamad@gmail.com>
2024-10-02 16:50:48 +0200

    add local evals experiment scripts

---
+++ examples/language-modeling/local_evals_experiments_scripts/llama_2_7b_mb-3.sh
@@ -0,0 +1,15 @@
+python3 main.py \
+    --model_name meta-llama/llama-2-7b-hf  \
+    --bits 4 \
+    --group_size 128 \
+    --eval_bs 32 \
+    --enable_lr_scheduler \
+    --nsamples 512 \
+    --iters 1000 \
+    --nblocks 3 \
+    --num_lookahead_blocks 0 \
+    --block_step_size 1 \
+    --eval_after_each_optimization \
+    --disable_eval \
+    --wandb_project_name khas-thesis-local-evals
+    
