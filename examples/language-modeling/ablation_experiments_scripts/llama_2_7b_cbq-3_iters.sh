69f1fb4d9e7175c11b92d413b5b75f1edfeb3c81
khasmamad99 <sh.khasmamad@gmail.com>
2024-10-02 14:26:50 +0200

    add more iterations to llama cbq-3 iters script

--- examples/language-modeling/ablation_experiments_scripts/llama_2_7b_cbq-3_iters.sh
+++ examples/language-modeling/ablation_experiments_scripts/llama_2_7b_cbq-3_iters.sh
@@ -86,4 +86,34 @@ python3 main.py \
     --block_step_size 1 \
     --num_lookahead_blocks 0 \
     --wandb_project_name khas-thesis-ablations
+
+
+python3 main.py \
+    --model_name meta-llama/llama-2-7b-hf  \
+    --bits 4 \
+    --group_size 128 \
+    --eval_bs 32 \
+    --lr 1e-3 \
+    --enable_lr_scheduler \
+    --nsamples 512 \
+    --iters 2000 \
+    --nblocks 3 \
+    --block_step_size 1 \
+    --num_lookahead_blocks 0 \
+    --wandb_project_name khas-thesis-ablations
+
+
+python3 main.py \
+    --model_name meta-llama/llama-2-7b-hf  \
+    --bits 4 \
+    --group_size 128 \
+    --eval_bs 32 \
+    --lr 1e-3 \
+    --enable_lr_scheduler \
+    --nsamples 512 \
+    --iters 2500 \
+    --nblocks 3 \
+    --block_step_size 1 \
+    --num_lookahead_blocks 0 \
+    --wandb_project_name khas-thesis-ablations
     
