b79cc87c5b31139af78f60f0af7f9d40bd2c12a1
khasmamad99 <sh.khasmamad@gmail.com>
2024-10-19 22:31:12 +0200

    add opt-6.7b mb main seeds exp scripts

---
+++ examples/language-modeling/main_experiment_scripts/v2/opt_6_7b_multiblock_seed=100.sh
@@ -0,0 +1,59 @@
+python3 main.py \
+    --model_name facebook/opt-6.7b  \
+    --bits 4 \
+    --group_size 128 \
+    --eval_bs 32 \
+    --enable_lr_scheduler \
+    --nsamples 512 \
+    --iters 1000 \
+    --nblocks 2 \
+    --block_step_size 2 \
+    --num_lookahead_blocks 0 \
+    --wandb_project_name khas-thesis-main \
+    --low_gpu_mem_usage \
+    --seed 100
+
+python3 main.py \
+    --model_name facebook/opt-6.7b  \
+    --bits 4 \
+    --group_size 128 \
+    --eval_bs 32 \
+    --enable_lr_scheduler \
+    --nsamples 512 \
+    --iters 1000 \
+    --nblocks 3 \
+    --block_step_size 3 \
+    --num_lookahead_blocks 0 \
+    --wandb_project_name khas-thesis-main \
+    --low_gpu_mem_usage \
+    --seed 100
+
+python3 main.py \
+    --model_name facebook/opt-6.7b  \
+    --bits 4 \
+    --group_size 128 \
+    --eval_bs 32 \
+    --enable_lr_scheduler \
+    --nsamples 512 \
+    --iters 1000 \
+    --nblocks 4 \
+    --block_step_size 4 \
+    --num_lookahead_blocks 0 \
+    --wandb_project_name khas-thesis-main \
+    --low_gpu_mem_usage \
+    --seed 100
+
+# python3 main.py \
+#     --model_name facebook/opt-6.7b  \
+#     --bits 4 \
+#     --group_size 128 \
+#     --eval_bs 32 \
+#     --enable_lr_scheduler \
+#     --nsamples 512 \
+#     --iters 1000 \
+#     --nblocks 5 \
+#     --block_step_size 5 \
+#     --num_lookahead_blocks 0 \
+#     --wandb_project_name khas-thesis-main \
+#     --low_gpu_mem_usage \
+# --seed 100
