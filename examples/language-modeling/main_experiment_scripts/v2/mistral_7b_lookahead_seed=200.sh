9eb89aa5e7596c3e1368bad8e476d5a265845276
khasmamad99 <sh.khasmamad@gmail.com>
2024-10-19 22:25:50 +0200

    mistral main scripts with different seeds

---
+++ examples/language-modeling/main_experiment_scripts/v2/mistral_7b_lookahead_seed=200.sh
@@ -0,0 +1,69 @@
+python3 main.py \
+    --model_name mistralai/Mistral-7B-v0.1  \
+    --bits 4 \
+    --group_size 128 \
+    --eval_bs 32 \
+    --enable_lr_scheduler \
+    --nsamples 512 \
+    --iters 1000 \
+    --nblocks 1 \
+    --block_step_size 1 \
+    --num_lookahead_blocks 0 \
+    --wandb_project_name khas-thesis-main \
+    --seed 100 
+
+python3 main.py \
+    --model_name mistralai/Mistral-7B-v0.1  \
+    --bits 4 \
+    --group_size 128 \
+    --eval_bs 32 \
+    --enable_lr_scheduler \
+    --nsamples 512 \
+    --iters 1000 \
+    --nblocks 1 \
+    --block_step_size 1 \
+    --num_lookahead_blocks 1 \
+    --wandb_project_name khas-thesis-main \
+    --seed 100
+
+python3 main.py \
+    --model_name mistralai/Mistral-7B-v0.1  \
+    --bits 4 \
+    --group_size 128 \
+    --eval_bs 32 \
+    --enable_lr_scheduler \
+    --nsamples 512 \
+    --iters 1000 \
+    --nblocks 1 \
+    --block_step_size 1 \
+    --num_lookahead_blocks 2 \
+    --wandb_project_name khas-thesis-main \
+    --seed 100
+
+python3 main.py \
+    --model_name mistralai/Mistral-7B-v0.1  \
+    --bits 4 \
+    --group_size 128 \
+    --eval_bs 32 \
+    --enable_lr_scheduler \
+    --nsamples 512 \
+    --iters 1000 \
+    --nblocks 1 \
+    --block_step_size 1 \
+    --num_lookahead_blocks 3 \
+    --wandb_project_name khas-thesis-main \
+    --seed 100
+
+# python3 main.py \
+#     --model_name mistralai/Mistral-7B-v0.1  \
+#     --bits 4 \
+#     --group_size 128 \
+#     --eval_bs 32 \
+#     --enable_lr_scheduler \
+#     --nsamples 512 \
+#     --iters 1000 \
+#     --nblocks 1 \
+# --block_step_size 1 \
+#     --num_lookahead_blocks 4 \
+#     --wandb_project_name khas-thesis-main \
+# --seed 0
---
+++ examples/language-modeling/main_experiment_scripts/v2/mistral_7b_multiblock_seed=100.sh
@@ -0,0 +1,55 @@
+python3 main.py \
+    --model_name mistralai/Mistral-7B-v0.1  \
+    --bits 4 \
+    --group_size 128 \
+    --eval_bs 32 \
+    --enable_lr_scheduler \
+    --nsamples 512 \
+    --iters 1000 \
+    --nblocks 2 \
+    --block_step_size 2 \
+    --num_lookahead_blocks 0 \
+    --wandb_project_name khas-thesis-main \
+    --seed 100
+
+python3 main.py \
+    --model_name mistralai/Mistral-7B-v0.1  \
+    --bits 4 \
+    --group_size 128 \
+    --eval_bs 32 \
+    --enable_lr_scheduler \
+    --nsamples 512 \
+    --iters 1000 \
+    --nblocks 3 \
+    --block_step_size 3 \
+    --num_lookahead_blocks 0 \
+    --wandb_project_name khas-thesis-main \
+    --seed 100
+
+python3 main.py \
+    --model_name mistralai/Mistral-7B-v0.1  \
+    --bits 4 \
+    --group_size 128 \
+    --eval_bs 32 \
+    --enable_lr_scheduler \
+    --nsamples 512 \
+    --iters 1000 \
+    --nblocks 4 \
+    --block_step_size 4 \
+    --num_lookahead_blocks 0 \
+    --wandb_project_name khas-thesis-main \
+    --seed 100
+
+# python3 main.py \
+#     --model_name mistralai/Mistral-7B-v0.1  \
+#     --bits 4 \
+#     --group_size 128 \
+#     --eval_bs 32 \
+#     --enable_lr_scheduler \
+#     --nsamples 512 \
+#     --iters 1000 \
+#     --nblocks 5 \
+#     --block_step_size 5 \
+#     --num_lookahead_blocks 0 \
+#     --wandb_project_name khas-thesis-main \
+# --seed 100
